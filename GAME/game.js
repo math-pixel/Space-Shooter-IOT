2ffdebdc0e29581d306a6213adc2270d9d50810e
math-pixel <mathcb74@gmail.com>
2024-03-25 15:51:39 +0100

    feat(spaceship) mouvement and fire

---
+++ GAME/game.js
@@ -0,0 +1,72 @@
+// Step 1: Setup
+let shipImg;
+let missileImg;
+let shipX, shipY;
+let missileX, missileY;
+let allMissile = []
+const missileSpeed = 5;
+const shipSpeed = 3;
+
+function preload() {
+  shipImg = loadImage('assets/ship.png');
+  missileImg = loadImage('assets/ship.png');
+}
+
+function setup() {
+  createCanvas(1000, 800);
+  shipX = width / 2;
+  shipY = height / 2;
+}
+
+// Step 2: Draw
+function draw() {
+    background(220);
+    
+    // Calculate skew values based on movement
+    let skewX = 0;
+    let skewY = 0;
+    if (keyIsDown(LEFT_ARROW)) {
+        shipX -= shipSpeed;
+        skewX = -0.1;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        shipX += shipSpeed;
+        skewX = 0.1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        shipY -= shipSpeed;
+        skewY = -0.1;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        shipY += shipSpeed;
+        skewY = 0.1;
+    }
+    
+    // Draw ship with skew
+    push();
+    translate(shipX, shipY);
+    shearX(skewX);
+    shearY(skewY);
+    imageMode(CENTER);
+    image(shipImg, 0, 0, 100, 100);
+
+    pop();
+  
+    // Draw missile
+    allMissile.forEach((currentMissile, index) => {
+        image(missileImg, currentMissile.x, currentMissile.y, 50, 50);
+        currentMissile.y -= missileSpeed;
+
+        if (currentMissile.y < -10){
+            allMissile.splice(index, 1);
+        }
+    })
+    print(allMissile)
+}
+
+// Step 4: Firing
+function keyTyped() {
+  if (key === ' ') {
+    allMissile.push({x: shipX, y: shipY})
+  }
+}
