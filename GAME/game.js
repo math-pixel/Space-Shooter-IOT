7242a6b2bba9007d3104e0ac0d4feba71df7fa07
math-pixel <mathcb74@gmail.com>
2024-03-25 16:01:37 +0100

    feat(enemy) basic spawn and walk forward auto

--- GAME/game.js
+++ GAME/game.js
@@ -7,6 +7,11 @@ let allMissile = []
 const missileSpeed = 5;
 const shipSpeed = 3;
 
+
+let ennemies = []
+let ennemieSpeed = 2
+
+
 function preload() {
   shipImg = loadImage('assets/ship.png');
   missileImg = loadImage('assets/ship.png');
@@ -61,7 +66,15 @@ function draw() {
             allMissile.splice(index, 1);
         }
     })
-    print(allMissile)
+    
+    ennemies.forEach((currentEnnemi, index) => {
+        image(missileImg, currentEnnemi.x, currentEnnemi.y, 50, 50);
+        currentEnnemi.y += currentEnnemi.speed;
+
+        if (currentEnnemi.y > 800){
+            ennemies.splice(index, 1);
+        }
+    })
 }
 
 // Step 4: Firing
@@ -69,4 +82,20 @@ function keyTyped() {
   if (key === ' ') {
     allMissile.push({x: shipX, y: shipY})
   }
+}
+
+function ennemieSpawn(interval){
+    setTimeout(() => {
+    
+        ennemies.push({x: getRandomArbitrary(0, 1000), y: 0, speed: ennemieSpeed})
+        ennemieSpawn( getRandomArbitrary(500, 2000) )
+
+    }, interval )
+}
+
+ennemieSpawn(1000)
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
