ee9aaa4226af0539fe9a06ab172cf135f97eb592
math-pixel <mathcb74@gmail.com>
2024-03-28 09:16:58 +0100

    update(HC04) convert to an object sensor

--- IOT/rpi/HC04_Sensor.py
+++ IOT/rpi/HC04_Sensor.py
@@ -4,50 +4,56 @@ import time
 # Définir les broches du capteur
 TRIG = 23
 ECHO = 24
+class HC04_Sensor:
 
-def setupHC04Sensor():
-    # Utiliser la numérotation BCM pour les broches GPIO
-    GPIO.setmode(GPIO.BCM)
+    def __init__(self, trig, echo) -> None:
+        self.trig = trig
+        self.echo = echo
 
-    # Définir TRIG comme une broche de sortie
-    GPIO.setup(TRIG, GPIO.OUT)
-    # Définir ECHO comme une broche d'entrée
-    GPIO.setup(ECHO, GPIO.IN)
+    def setupHC04Sensor(self):
+        # Utiliser la numérotation BCM pour les broches GPIO
+        GPIO.setmode(GPIO.BCM)
 
-def get_distance():
-    # Envoyer une impulsion ultrasonique
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+        # Définir TRIG comme une broche de sortie
+        GPIO.setup(self.trig, GPIO.OUT)
+        # Définir ECHO comme une broche d'entrée
+        GPIO.setup(self.echo, GPIO.IN)
 
-    start_time = time.time()
-    stop_time = time.time()
+    def get_distance(self):
+        # Envoyer une impulsion ultrasonique
+        GPIO.output(self.trig, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
-    # Enregistrer le temps de début
-    while GPIO.input(ECHO) == 0:
         start_time = time.time()
-
-    # Enregistrer le temps d'arrêt
-    while GPIO.input(ECHO) == 1:
         stop_time = time.time()
 
-    # Calculer la durée de l'impulsion ultrasonique
-    elapsed_time = stop_time - start_time
+        # Enregistrer le temps de début
+        while GPIO.input(self.echo) == 0:
+            start_time = time.time()
 
-    # Calculer la distance en utilisant la vitesse du son (343m/s)
-    # La distance est la moitié de la durée de l'impulsion multipliée par la vitesse du son
-    distance = (elapsed_time * 34300) / 2
+        # Enregistrer le temps d'arrêt
+        while GPIO.input(self.echo) == 1:
+            stop_time = time.time()
 
-    return distance
+        # Calculer la durée de l'impulsion ultrasonique
+        elapsed_time = stop_time - start_time
+
+        # Calculer la distance en utilisant la vitesse du son (343m/s)
+        # La distance est la moitié de la durée de l'impulsion multipliée par la vitesse du son
+        distance = (elapsed_time * 34300) / 2
+
+        return distance
 
 def cleanup():
     GPIO.cleanup()
 
 if __name__ == '__main__':
     try:
-        setupHC04Sensor()
+        sensor = HC04_Sensor(TRIG, ECHO)
+        sensor.setupHC04Sensor()
         while True:
-            distance = get_distance()
+            distance = sensor.get_distance()
             print("Distance:", distance, "cm")
             time.sleep(1)
     except KeyboardInterrupt:
