ca1dc3cc69202efcaa4b3eb7b13fdf77961734e5
math-pixel <mathcb74@gmail.com>
2024-03-29 09:35:33 +0100

    feat(MPU5060) new code for sensor mpu5060

--- IOT/myEsp32/MPU5060_sensor.py
+++ IOT/myEsp32/MPU5060_sensor.py
@@ -1,29 +1,46 @@
-from machine import Pin, I2C
-from mpu6050 import MPU6050
+import machine
 
 class Gyro_Sensor():
+    def __init__(self, i2c, addr=0x68):
+        self.iic = i2c
+        self.addr = addr
+        self.iic.start()
+        self.iic.writeto(self.addr, bytearray([107, 0]))
+        self.iic.stop()
 
-    def __init__(self, sclPin=22, sdaPin=21) -> None:
-        # Setup I2C
-        self.i2c = I2C(scl=Pin(sclPin), sda=Pin(sdaPin))
-        # Initialize MPU6050
-        self.mpu = MPU6050(self.i2c)
-        # Test MPU6050 connection
-        print("Testing MPU6050 connection...")
-        print("MPU6050 connection successful" if self.mpu.whoami() == 0x68 else "MPU6050 connection failed")
+    def get_raw_values(self):
+        self.iic.start()
+        a = self.iic.readfrom_mem(self.addr, 0x3B, 14)
+        self.iic.stop()
+        return a
 
-    def getGyroData(self):
-        gyro_data = self.mpu.get_gyro_data()
-        return gyro_data
-    
-    def getAccelData(self):
-        accel_data = self.mpu.get_accel_data() 
-        return accel_data
-    
-# Read and print sensor data
-# while True:
-#     accel_data = mpu.get_accel_data()
-#     gyro_data = mpu.get_gyro_data()
-#     print("Accel: {}, {}, {}".format(accel_data['x'], accel_data['y'], accel_data['z']))
-#     print("Gyro: {}, {}, {}".format(gyro_data['x'], gyro_data['y'], gyro_data['z']))
-#     time.sleep(1)
+    def get_ints(self):
+        b = self.get_raw_values()
+        c = []
+        for i in b:
+            c.append(i)
+        return c
+
+    def bytes_toint(self, firstbyte, secondbyte):
+        if not firstbyte & 0x80:
+            return firstbyte << 8 | secondbyte
+        return - (((firstbyte ^ 255) << 8) | (secondbyte ^ 255) + 1)
+
+    def get_values(self):
+        raw_ints = self.get_raw_values()
+        vals = {}
+        vals["AcX"] = self.bytes_toint(raw_ints[0], raw_ints[1])
+        vals["AcY"] = self.bytes_toint(raw_ints[2], raw_ints[3])
+        vals["AcZ"] = self.bytes_toint(raw_ints[4], raw_ints[5])
+        vals["Tmp"] = self.bytes_toint(raw_ints[6], raw_ints[7]) / 340.00 + 36.53
+        vals["GyX"] = self.bytes_toint(raw_ints[8], raw_ints[9])
+        vals["GyY"] = self.bytes_toint(raw_ints[10], raw_ints[11])
+        vals["GyZ"] = self.bytes_toint(raw_ints[12], raw_ints[13])
+        return vals  # returned in range of Int16
+        # -32768 to 32767
+
+    def val_test(self):  # ONLY FOR TESTING! Also, fast reading sometimes crashes IIC
+        from time import sleep
+        while 1:
+            print(self.get_values())
+            sleep(0.05)
--- IOT/myEsp32/main.py
+++ IOT/myEsp32/main.py
@@ -2,6 +2,7 @@ from time import sleep_ms
 from SocketIOManager import *
 from MPU5060_logic import *
 from MPU5060_sensor import *
+from machine import I2C, Pin
 
 server_url = 'http://localhost:8000'
 manager = SocketIOClientManager(server_url)
@@ -11,12 +12,18 @@ manager.sendMessage("message", {"msg": "Hello from ESP32!"})
 threshold = 10
 logicSensor = GYRO(threshold)
 
-sensorGyro = Gyro_Sensor()
+i2c = I2C(scl=Pin(22), sda=Pin(21))     #initializing the I2C method for ESP32
+sensorGyro = Gyro_Sensor(i2c)
 
 while True:
     try:
         print("routine")
-        logicSensor.process(sensorGyro.getGyroData())
+        sensorGyro.get_values()
+        print(sensorGyro.get_values())
+
+        X = 0 # set with gyro Y
+        Y = 0 # set with gyro X
+        logicSensor.process({"x": X, "y":Y}) #sensorGyro.getGyroData()
         sleep_ms(100)
   
     except KeyboardInterrupt:
