b0ebadfe14d5156f81bc6f4e338d5015ed3fcc5d
math-pixel <mathcb74@gmail.com>
2024-03-29 09:37:43 +0100

    update(comment) Add comment for esp main.py

--- IOT/myEsp32/main.py
+++ IOT/myEsp32/main.py
@@ -4,17 +4,21 @@ from MPU5060_logic import *
 from MPU5060_sensor import *
 from machine import I2C, Pin
 
-server_url = 'http://localhost:8000'
-manager = SocketIOClientManager(server_url)
-manager.setup_event_handlers()
-manager.sendMessage("message", {"msg": "Hello from ESP32!"})
+# --------------------------------- Websocket -------------------------------- #
+# server_url = 'http://localhost:8000'
+# manager = SocketIOClientManager(server_url)
+# manager.setup_event_handlers()
+# manager.sendMessage("message", {"msg": "Hello from ESP32!"})
 
+# ------------------------------- Logic Sensor ------------------------------- #
 threshold = 10
 logicSensor = GYRO(threshold)
 
+# -------------------------------- Real Sensor ------------------------------- #
 i2c = I2C(scl=Pin(22), sda=Pin(21))     #initializing the I2C method for ESP32
 sensorGyro = Gyro_Sensor(i2c)
 
+# -------------------------------- Loop esp32 -------------------------------- #
 while True:
     try:
         print("routine")
